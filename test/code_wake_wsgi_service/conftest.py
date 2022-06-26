3587abe3dcc7413ba0e885edd54b31a044fcc789
Michael Wright <mjw@methodanalysis.com>
2022-06-26 22:17:02 +0100

    release 0.0.1

---
+++ test/code_wake_wsgi_service/conftest.py
@@ -0,0 +1,44 @@
+"""Pytest configuration / fixtures."""
+
+
+import flask
+import flask.testing
+import pytest
+from code_wake.test.conftest import *
+from code_wake_sql14_store import Sql14Store
+from code_wake_v1rest_store import V1RestStore
+
+from code_wake_v1wsgi_service import V1WsgiMiddleware
+
+
+@pytest.fixture
+def store_params():
+    def params():
+        flask_app = flask.Flask(__name__)
+        flask_app.wsgi_app = V1WsgiMiddleware(flask_app.wsgi_app, "/abc", Sql14Store("sqlite:///:memory:"))
+
+        return (["/abc"], {"flask_app": flask_app})
+
+    return params
+
+
+@pytest.fixture
+def store_cls():
+    class TestV1RestStore(V1RestStore):
+        def __init__(self, *args, flask_app, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self._flask_app = flask_app
+            flask_app.test_client_class = self.FlaskClient
+
+        class FlaskClient(flask.testing.FlaskClient):
+            def open(self, *args, **kwargs):
+                headers = kwargs.setdefault("headers", {})
+                headers.setdefault("content-type", "application/json")
+                return super().open(*args, **kwargs)
+
+        @property
+        def session(self):
+            return self._flask_app.test_client
+
+    return TestV1RestStore
---
+++ test/code_wake_wsgi_service/test_v1wsgi_service.py
@@ -0,0 +1,4 @@
+"""V1 WSGI Werkzeug middleware tests module."""
+
+
+from code_wake.test.store import *
