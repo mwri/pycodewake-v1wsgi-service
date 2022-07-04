0dd4dbcf4d53bb36252d9824ecb8e27721420fcf
Michael Wright <mjw@methodanalysis.com>
2022-07-04 01:17:01 +0100

    release 0.1.1

--- code_wake_v1wsgi_service/pkg_meta.py
+++ code_wake_v1wsgi_service/pkg_meta.py
@@ -4,7 +4,7 @@ import sys
 from typing import Any, Dict
 
 name = "code-wake-v1wsgi-service"
-version = "0.0.2"
+version = "0.1.1"
 author = "Michael Wright"
 author_email = "mjw@methodanalysis.com"
 description = "Code Wake V1 WSGI Werkzeug middleware"
